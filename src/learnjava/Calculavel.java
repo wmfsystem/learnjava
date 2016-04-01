410b2b19870bf6d1d316c0b5335de3d022eca80f
Fernando Paschualetto <paschualetto@gmail.com>
2016-04-01 19:24:19 -0300

    Calculadora Java

---
+++ src/learnjava/Calculavel.java
@@ -0,0 +1,16 @@
+package learnjava;
+
+/**
+ *
+ * @author fernando
+ */
+public interface Calculavel {
+
+    double somar(double... numeros);
+
+    double subtrair(double... numeros);
+
+    double dividir(double... numeros);
+
+    double multiplicar(double... numeros);
+}
--- src/learnjava/LearnJava.java
+++ src/learnjava/LearnJava.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package learnjava;
 
 /**
@@ -17,5 +12,5 @@ public class LearnJava {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
